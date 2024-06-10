62ef24298f5a000b23a0040ee76ea51f7034cd68
Sadiq Lətifli <sadiq.latifli@optima.az>
2024-06-10 10:35:09 +0400

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const withNextIntl = createNextIntlPlugin('./src/i18n.js');
  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: 'dist',
     images: {
         remotePatterns: [
             {protocol: "https", hostname: '**'}
